962f40a4927c9a346ed7aded06ab3c4effafaf4c
Michael Chung <michael061195@gmail.com>
2024-07-20 14:32:18 -0500

    button form

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -3,8 +3,18 @@ import React from "react";
 function Form(props) {
   return (
     <form className="chore-form">
-      <input className="input-chore">
+      <input
+      className="input-chore"
+      placeholder="Add a chore"
+      name='text'
+      type="text">
+      </input>
+      <button
+      className="chore-button">
+
+      </button>
     </form>
+
   )
 
 }
